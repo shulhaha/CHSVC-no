0067c0af9f381365db187a5439633d1aedbe46fb
shulhaha <shulhaha@protonmail.com>
2023-07-05 23:35:01 +0300

    init!

---
+++ js/reflink.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("a").click(function(){
+    $("#22bet*").attr("href", "https://refpasrasw.world/L?tag=d_1137603m_7669c_&site=1137603&ad=7669&r=it/slots");
+    $("#LokiCasino*").attr("href", "https://atraff.com/?serial=12989&creative_id=705&anid=");
+    $("#EUslot*").attr("href", "https://atraff.com/?serial=12989&creative_id=707&anid=");
+    $("#20bet*").attr("href", "https://promo.20bet.partners/redirect.aspx?pid=11112&bid=1971&lpid=281&sref=Revlab&Revlab=20bet_20bet170");
+    $("#NationalCasino*").attr("href", "https://media.playamopartners.com/redirect.aspx?pid=11112&bid=2036&lpid=502&sref=Revlab&Revlab=national_national188");
+    $("#SpinSamurai*").attr("href", "https://partnersredirect.com/a186f3e4e");
+    $("#CasinoIn*").attr("href", "https://record.affiliatesbm2.com/_7YNYmyvW0AMdimWE6PdS1WNd7ZgqdRLk/1/");
+	
+  });
+});
